a926cc6623edb9ac8dfca27c9027be46f310ad67
antalkon <antalkon789@gmail.com>
2024-06-05 15:53:44 +0500

    final preview

---
+++ 1biology/script/carusel.js
@@ -0,0 +1,27 @@
+// script.js
+
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-image');
+    const totalSlides = slides.length;
+
+    if (index >= totalSlides) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalSlides - 1;
+    } else {
+        currentIndex = index;
+    }
+
+    const newTransform = `translateX(-${currentIndex * 100}%)`;
+    document.querySelector('.carousel-inner').style.transform = newTransform;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
